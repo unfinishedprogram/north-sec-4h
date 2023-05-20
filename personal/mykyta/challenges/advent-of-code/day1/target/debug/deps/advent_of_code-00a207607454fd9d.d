5f498136bba69e884c6382e9a857acb1b31e0221
nenikitov <nenikitov@mail.ru>
2023-05-19 20:31:51 -0400

    Added gitignore

--- personal/mykyta/challenges/advent-of-code/day1/target/debug/deps/advent_of_code-00a207607454fd9d.d
+++
@@ -1,6 +0,0 @@
-/home/nenikitov/SharedFiles/College/6/CTF/NorthSec/north-sec-4h/personal/mykyta/challenges/advent-of-code/target/debug/deps/advent_of_code-00a207607454fd9d.rmeta: src/main.rs src/../input.txt
-
-/home/nenikitov/SharedFiles/College/6/CTF/NorthSec/north-sec-4h/personal/mykyta/challenges/advent-of-code/target/debug/deps/advent_of_code-00a207607454fd9d.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
--- personal/mykyta/challenges/advent-of-code/day1/target/debug/deps/advent_of_code-e0e87956cc3e86ef.d
+++
@@ -1,6 +0,0 @@
-/home/nenikitov/SharedFiles/College/6/CTF/NorthSec/north-sec-4h/personal/mykyta/challenges/advent-of-code/day1/target/debug/deps/advent_of_code-e0e87956cc3e86ef: src/main.rs src/../input.txt
-
-/home/nenikitov/SharedFiles/College/6/CTF/NorthSec/north-sec-4h/personal/mykyta/challenges/advent-of-code/day1/target/debug/deps/advent_of_code-e0e87956cc3e86ef.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
