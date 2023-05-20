4b92a3303aee603204f4401aacf3f8b12323826b
Marian-Hristov <marianhr2014@gmail.com>
2023-05-19 21:10:05 -0400

    misspelled something

--- vpn-connect.sh
+++ vpn-connect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nmcli connection import type openvpn file team-008.openvpn
+nmcli connection import type openvpn file team-008.ovpn
 nmcli connection modify team-008 ipv4.never-default true ipv6.never-default true
 nmcli connection up team-008
