ee8b107f843d30e18a820a4f8ae65cfdca870dc8
nenikitov <nenikitov@mail.ru>
2023-05-19 20:55:27 -0400

    Added gitignore

--- personal/mykyta/challenges/advent-of-code/day_2/target/debug/deps/day_2-a86718b0970c9ffc.d
+++
@@ -1,6 +0,0 @@
-/home/nenikitov/SharedFiles/College/6/CTF/NorthSec/north-sec-4h/personal/mykyta/challenges/advent-of-code/day_2/target/debug/deps/day_2-a86718b0970c9ffc.rmeta: src/main.rs src/../input.txt
-
-/home/nenikitov/SharedFiles/College/6/CTF/NorthSec/north-sec-4h/personal/mykyta/challenges/advent-of-code/day_2/target/debug/deps/day_2-a86718b0970c9ffc.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
